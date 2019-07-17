f1234572c12217e3760d7fd6ff1e9e3ea4218ee0
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-07-17 18:35:11 -0400

    basic styling complete

--- src/Component.js
+++ src/Component.js
@@ -4,10 +4,15 @@ import React from 'react';
 const Component = (props) => {
 
     return (
-        <div>
-            <iframe src={props.url}></iframe>
-            <h2>{props.title}</h2>
-            <p>{props.explanation}</p>
+        <div className="appContainer">
+            <div className="imgContainer">
+                <iframe src={props.url}></iframe>
+            </div>
+
+            <div className="textContainer">
+                <h2 className="titleText">{props.title}</h2>
+                <p className="expText">{props.explanation}</p>
+            </div>
         </div>
     )
 }
